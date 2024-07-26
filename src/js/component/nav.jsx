0b13d382897048745858df44bf92dbfbdca2d20b
Jahn1295 <jiheeahn1222@gmail.com>
2024-07-26 23:00:33 +0000

    done1

--- src/js/component/nav.jsx
+++ src/js/component/nav.jsx
@@ -13,7 +13,12 @@ export function Nav(props) {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="collapse navbar-collapse"
+                    style={{
+                        display:"flex",
+                        justifyContent:"end"
+                    }}
+                    id="navbarNavAltMarkup">
                     <div className="navbar-nav">
                         <a className="nav-link active" aria-current="page" href="#">{homePage}</a>
                         <a className="nav-link" href="#">{myAccount}</a>
